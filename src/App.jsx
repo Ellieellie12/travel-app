7a4c0cc27af596750cdf91432a108d3dd5f4a5b5
Ellie <eagyeman1234@gmail.com>
2023-11-28 15:43:59 -0500

    add NavBar link and import NavBar into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+//npm packages
+// import React from 'react'
+
+//npm modules
+import {Routes, Route} from 'react-router-dom'
+
+//components
+import NavBar from './components/NavBar'
+
+//styles
 import './App.css'
+
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+    <NavBar />
+        <Routes>
+          <Route path ='/' />
+
+
+
+
+        </Routes>
+
+      
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+//npm packages
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter as Router} from 'react-router-dom'
 
+//app
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
