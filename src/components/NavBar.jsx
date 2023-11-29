ef22c50a3dfed61526e4614181b23b92de8c3d44
Ellie <eagyeman1234@gmail.com>
2023-11-29 09:32:45 -0500

    add links for home, services, and products

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,38 @@ import { Link } from 'react-router-dom'
 
 
 function NavBar() {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
   return (
   <>
     <nav className='navbar'>
       <div className='navbar-container'>
         <Link to='/' className="navbar-logo">
-          TRAVEL <i className="fa-solid fa-passport"></i>
+          TRAVEL <i className="fa-solid fa-passport" />
         </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Services
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Products
+            </Link>
+          </li>
+
+        </ul>
       </div>
     </nav>
   </>
