ca71a00ef129e2569b654d39cbf343ad08718131
Ellie <eagyeman1234@gmail.com>
2023-12-06 14:42:11 -0500

    import cards to home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,14 @@
 // import React from 'react'
 import '../../App.css'
 import VideoSection from '../VideoSection/VideoSection'
+import Cards from '../card/Cards'
+
 
 function Home () {
   return(
     <>
       <VideoSection />
+      <Cards />
     </>
   )
 }
