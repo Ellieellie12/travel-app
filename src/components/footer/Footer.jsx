00a6932637599af212e29e9a66054e917258ac47
Ellie <eagyeman1234@gmail.com>
2023-12-06 19:55:39 -0500

    add links for instagram and facebook

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,6 @@ function Footer() {
         </p>
         <p className='footer-subscription-text'>
           You can unsubscribe at any time.
-
         </p>
         <div className="input-areas">
           <form>
@@ -30,8 +29,8 @@ function Footer() {
         </div>
       </section>
       <div className="footer-links">
-        <div className="footer-link-wrapper">
-          <div className="footer-link-items">
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
@@ -39,8 +38,56 @@ function Footer() {
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
+          <div className='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
         </div>
       </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              TRAVEL <i className='fa-solid fa-passport' />
+            </Link>
+          </div>
+          <small className="website-rights">TRAVEL</small>
+          <div className="social-icons">
+            <Link className="social-icon-link facebook" 
+            to='/'
+            target='blank'
+            aria-label='Facebook'>
+              <i className='fab fa-facebook-f'></i>
+            </Link>
+            <Link className="social-icon-link instagram" 
+            to='/'
+            target='blank'
+            aria-label='Instagram'>
+              <i className='fab fa-instagram'></i>
+            </Link>
+          </div>
+
+        </div>
+      </section>
     </div>
   )
 }
